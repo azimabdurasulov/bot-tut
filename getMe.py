e560dbb966c65a2e17d5ccd0d48643cda5bdc1f3
djumanov <diyorbekjumanov1120@gmail.com>
2022-12-21 19:04:50 +0500

    send msg

--- getMe.py
+++ getMe.py
@@ -2,13 +2,29 @@ import requests
 
 TOKEN = '5888802954:AAG1jeTE-CAQD2H7hFRje-CH9Yj_Yd6I7_o'
 
-response = requests.get(f'https://api.telegram.org/bot{TOKEN}/getUpdates')
+def get_updates(TOKEN):
+    response = requests.get(f'https://api.telegram.org/bot{TOKEN}/getUpdates')
 
-if response.status_code == 200:
-    data = response.json()
-    updates = data['result']
-    for update in updates:
-        msg = update['message']
-        text = msg['text']
-        user = msg['from']
-        print(user)
+
+    if response.status_code == 200:
+        data = response.json()
+        updates = data['result']
+        for update in updates:
+            msg = update['message']
+            text = msg['text']
+            user = msg['from']
+            print(user['id'])
+
+get_updates(TOKEN)
+
+
+def send_msg(TOKEN, payload):
+    response = requests.get(f'https://api.telegram.org/bot{TOKEN}/sendMessage', params=payload)
+
+
+payload = {
+    'chat_id': '1258594598',
+    'text': 'Welcome to my bot!'
+}
+
+send_msg(TOKEN, payload)
