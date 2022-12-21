9102a5329c8653bced67e8c571d1efa6e6361867
djumanov <diyorbekjumanov1120@gmail.com>
2022-12-21 18:49:34 +0500

    get updates

---
+++ getMe.py
@@ -0,0 +1,12 @@
+import requests
+
+TOKEN = '5888802954:AAG1jeTE-CAQD2H7hFRje-CH9Yj_Yd6I7_o'
+
+response = requests.get(f'https://api.telegram.org/bot{TOKEN}/getUpdates')
+
+if response.status_code == 200:
+    data = response.json()
+    updates = data['result']
+    for update in updates:
+        msg = update['message']['text']
+        print(msg)
