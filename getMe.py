f6d2399e66ce676158a3c2d50f5ccbab75211a76
azimabdurasulov <azimjon20050604@gmail.com>
2023-01-04 09:17:21 +0500

    echo_bot

--- getMe.py
+++ getMe.py
@@ -22,11 +22,11 @@ def send_message(chat_id, text):
     }
     response = requests.get(f'https://api.telegram.org/bot{TOKEN}/sendMessage', params=payload)
 
-last_update_id, _, _ = last_update_data()
+last_update_id = -1
 
 while True:
     current_update_id, chat_id, text = last_update_data()
-
+    # print(f"update: {last_update_id}, last update: {current_update_id}")
     if last_update_id != current_update_id:
         send_message(chat_id, text)
         last_update_id = current_update_id
